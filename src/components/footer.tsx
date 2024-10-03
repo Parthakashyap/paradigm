9b8c1f8dc6f8ee2d61ee6861db601cb64b454574
Partha Kashyap <parthakashyal@gmail.com>
2024-10-03 17:21:38 +0530

    size

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -58,7 +58,7 @@ const Footer = () => {
       </div>
 
       {/* Footer Bottom Section */}
-      <div className="mt-10 text-sm font-mono text-left">
+      <div className="mt-10 text-[.75rem] font-mono text-left">
         <p>Copyright © 2024 Aegean Operations LP All rights reserved.
           “Aegean” is a trademark, and the triangular mobius symbol is a
           registered trademark of Aegean Operations LP
