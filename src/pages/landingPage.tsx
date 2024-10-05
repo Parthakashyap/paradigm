2a1cf2fa4fe43ea04cd4122254ae45a7ed115d3b
Partha Kashyap <parthakashyal@gmail.com>
2024-10-05 16:40:03 +0530

    Update link styling in landing page

--- src/pages/landingPage.tsx
+++ src/pages/landingPage.tsx
@@ -69,7 +69,7 @@ const LandingPage = () => {
         />
       </div>
 
-      <Link href={"/info"} className="text-lg mb-10">
+      <Link href={"/info"} className="text-lg mb-16">
         Aegean Capital
       </Link>
     </div>
